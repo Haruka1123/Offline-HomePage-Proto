c3ab6591fb5c556393d66aacbdde020f17b13fab
Haruka1123 <juamigz.arimohanan@gmail.com>
2024-12-16 01:07:53 +1100

    Add files via upload

---
+++ Service Workers sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'home-page-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html', // Login Page
+    '/Home-Page/LMS.html', // Home Page
+    '/style.css',   // CSS file
+    '/app.js',      // JavaScript file for service worker registration
+    // Add any other assets you want to cache (images, etc.)
+];
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// Fetch from cache or network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate the service worker
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
